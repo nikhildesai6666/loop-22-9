37a4a7c1de0cba44747880ebb00f874142eadd09
nikhildesai6666 <desainikhil9664@gmail.com>
2022-09-22 11:51:57 +0530

    Add project files.

---
+++ loop program22/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_program22
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            //54321
+            //5432
+            //543
+            //54
+            //5
+
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 5; j >= i; j++)
+                {
+                    Console.WriteLine("i");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ loop program22/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_program22
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            //12345
+            //1234
+            //123
+            //12
+            //1
+
+            for (int i = 1; i <=5; i--)
+            {
+                for (int  j = 1;  j <=i;  j--)
+                {
+                    Console.WriteLine("i");
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
